5984ecb66bf941fcb05fb68152beb58f42606d59
AhadSajjad <imahadsajjad@gmail.com>
2022-12-24 17:24:21 +0500

    onboarding final pls

--- lib/Onboarding.dart
+++ lib/Onboarding.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import "package:onboarding/onboarding.dart";
-import 'package:flutter/cupertino.dart';
+
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -11,7 +11,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
 
   late Material materialButton;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,8 +36,8 @@ class _MyAppState extends State<MyApp> {
                     SizedBox(height: 50),
                     Text("WELCOME TO DEEZ RAILWAYS!", style: TextStyle(color: Colors.white, fontSize: 30),),
                     SizedBox(height: 50),
-                  Image.asset('thomas.png',height: 300,width: 500),
-              ],
+                    FadeInImage.assetNetwork(placeholder: "assets/thomas.png",height: 300 , image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d5a30244-7dcd-4acc-a472-cbd2bd37a1dc/d9bzdfr-a8f3182d-af2e-458c-8911-4e31f2764fda.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q1YTMwMjQ0LTdkY2QtNGFjYy1hNDcyLWNiZDJiZDM3YTFkY1wvZDliemRmci1hOGYzMTgyZC1hZjJlLTQ1OGMtODkxMS00ZTMxZjI3NjRmZGEuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.aTnZYYN_dCMj50kuLcAddNCOl-Kp3iI8yN-V6G5B_l4',width: 500),
+                  ],
               ),
               ),
               Center(
@@ -113,3 +112,4 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
